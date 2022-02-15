7c0f9cec2a9199e87dcce8f76134488a411f0036
Gleidson <gleidisonmt@gmail.com>
2022-02-15 18:07:06 -0300

    update

---
+++ src/main/java/io/github/gleidsonmt/speedcut/core/layout/CenterLayout.java
@@ -0,0 +1,46 @@
+/*
+ *    Copyright (C) Gleidson Neves da Silveira
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.gleidsonmt.speedcut.core.layout;
+
+import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  15/02/2022
+ */
+public class CenterLayout extends VBox {
+
+    private final ScrollPane    body    = new ScrollPane();
+    private final Bar           bar     = new Bar();
+
+    public CenterLayout() {
+        getChildren().add(0, bar);
+        getChildren().add(1, body);
+        body.setFitToWidth(true);
+        body.setFitToHeight(true);
+        VBox.setVgrow(body, Priority.ALWAYS);
+
+    }
+
+    public void setBody(Parent body) {
+        this.body.setContent(body);
+    }
+}
